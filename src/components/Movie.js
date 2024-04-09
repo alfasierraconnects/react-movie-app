daa4f2e2f17fe1a9d4ac7399f6be3e96a8473969
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-09 23:47:13 +0530

    Added retrying logic when fetch fails

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,19 +4,39 @@ import Item from "./Item";
 const Movie = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [retryInterval, setRetryInterval] = useState(null);
 
   const fetchMoviesHandler = () => {
     setIsLoading(true);
     fetch("https://dummyapi.online/api/movies/")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((json) => {
         setMovieList(json);
         setIsLoading(false);
+        clearTimeout(retryInterval); // Clear retry interval if fetch is successful
       })
       .catch((error) => {
-        setIsLoading(false);
-        console.error("Error fetching movies:", error);
+        retryFetchingMovies();
       });
+  };
+
+  const retryFetchingMovies = () => {
+    setIsLoading(true);
+    setRetryInterval(
+      setTimeout(() => {
+        fetchMoviesHandler();
+      }, 5000)
+    );
+  };
+
+  const cancelRetry = () => {
+    setIsLoading(false);
+    clearTimeout(retryInterval);
   };
 
   return (
@@ -27,14 +47,20 @@ const Movie = () => {
       >
         Fetch
       </button>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        {isLoading && (
-          <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 bg-black">
-            <div className="animate-spin text-white p-10 text-2xl font-extrabold">
-              .
-            </div>
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 bg-black">
+          <div className="animate-spin text-white p-10 text-2xl font-extrabold">
+            .
           </div>
-        )}
+          <button
+            className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={cancelRetry}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {!isLoading && movieList.map((el) => <Item key={el.id} movie={el} />)}
       </div>
     </div>
